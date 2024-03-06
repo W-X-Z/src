eae94f66e5eafc83178ee143767c205c91b7e1be
W-X-Z <116233351+W-X-Z@users.noreply.github.com>
2024-03-06 09:44:55 +0900

    차트 로직 추가

--- jumpman/index.js
+++ jumpman/index.js
@@ -1,24 +1,72 @@
-// Import JS files in need
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> <!-- Chart.js 라이브러리 추가 -->
+    <title>퀀텀점프</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background-color: #000000;
+        }
+        #fullscreen {
+            box-sizing: border-box;
+            width: 100vw;
+            height: 100vh;
+            padding-top: 10vh;
+        }
+        .gamearea {
+            margin-left: auto;
+            margin-right: auto;
+            width: 80vw;
+            height: 80vh;
+            border: 2px solid black;
+            background-color: #000000;
+        }
+        canvas#gameCanvas {
+            display: block;
+            margin: 0 auto;
+        }
+        canvas#jumpChart {
+            display: block;
+            margin: 0 auto; /* 기본 마진 설정 */
+            margin-top: -32vh; /* PC 환경에서 차트 위치 조정 */
+        }
+        /* Media Query for Mobile Devices */
+        @media (max-width: 768px) {
+            #fullscreen {
+                padding-top: 5vh;
+            }
+            .gamearea {
+                width: 90vw;
+                height: 70vh;
+            }
+            canvas#gameCanvas,
+            canvas#jumpChart {
+                width: 90vw;
+                margin: 0 auto;
+                margin-top: -32vh; /* 모바일 환경에서 차트 위치 조정 */
+            }
+        }
+        /* Media Query for Tablets */
+        @media (min-width: 769px) and (max-width: 1024px) {
+            canvas#jumpChart {
+                margin-top: -32vh; /* 태블릿 환경에서 차트 위치 조정 */
+            }
+        }
+        /* 추가 미디어 쿼리를 이용하여 더 많은 디바이스 범위에 대해 적용 가능 */
+    </style>
+</head>
+<body>
+    <div id="fullscreen">
+        <div id="JumpMan" class="gamearea"></div>
+    </div>
 
-const loadScriptFile = (filename) => {
-    if(!filename) return
-    
-    const bodyElement = document.getElementsByTagName("body")[0];
-    const scriptElement = document.createElement("script");
-    scriptElement.setAttribute("type", "text/javascript");
-    scriptElement.setAttribute("src", filename);
-    bodyElement.appendChild(scriptElement);
-}
-
-const scriptsToLoad = [
-    "jumpman/scripts/init.js",
-    "jumpman/scripts/setting.js",
-    "jumpman/scripts/physics.js",
-    "jumpman/scripts/world.js",
-    "jumpman/scripts/player.js",
-    "jumpman/scripts/maps.js",
-    "jumpman/scripts/loader.js",
-]
-
-let loadIndex = 0
-loadScriptFile(scriptsToLoad[loadIndex])
+    <canvas id="gameCanvas" width="800" height="600"></canvas>
+    <canvas id="jumpChart" width="800" height="400"></canvas> <!-- 점프 데이터 차트를 위한 canvas -->
+    <script type="text/javascript" src="jumpman/index.js"></script>
+</body>
+</html>
